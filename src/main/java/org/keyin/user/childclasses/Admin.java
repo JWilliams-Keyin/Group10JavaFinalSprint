54131d4abcbe77ed1e0e229a20bf5203afabb4de
JWilliams-Keyin <Jack.Williams@keyin.com>
2025-04-03 17:21:10 -0230

    Added new attributes to Admin.java

--- src/main/java/org/keyin/user/childclasses/Admin.java
+++ src/main/java/org/keyin/user/childclasses/Admin.java
@@ -3,10 +3,10 @@ package org.keyin.user.childclasses;
 import org.keyin.user.User;
 
 public class Admin extends User {
-  
+
     // Constructor
 
-    public Admin(String username, String password, String email, int phoneNumber, String address) {
-        super(username, password, email, phoneNumber, address, "Admin");
+    public Admin(String username, String password, String email, String phoneNumber, String address, String emergencyContactName, String emergencyContactPhoneNumber) {
+        super(username, password, email, phoneNumber, address, "Admin", emergencyContactName, emergencyContactPhoneNumber);
     }
 }
