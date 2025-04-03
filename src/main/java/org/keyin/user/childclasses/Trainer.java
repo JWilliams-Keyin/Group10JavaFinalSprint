7071becd664027aba0fda129b2a51bd7741ac820
JWilliams-Keyin <Jack.Williams@keyin.com>
2025-04-03 17:26:33 -0230

    Added new names and attributes to Trainer.java

--- src/main/java/org/keyin/user/childclasses/Trainer.java
+++ src/main/java/org/keyin/user/childclasses/Trainer.java
@@ -3,10 +3,10 @@ package org.keyin.user.childclasses;
 import org.keyin.user.User;
 
 public class Trainer extends User {
-  
+
     // Constructor
 
-    public Trainer(String username, String password, String email, int phoneNumber, String address) {
-        super(username, password, email, phoneNumber, address, "Trainer");
+    public Trainer(String userName, String userPassword, String userEmail, String userPhoneNumber, String userAddress, String userEmergencyContactName, String userEmergencyContactPhoneNumber) {
+        super(userName, userPassword, userEmail, userPhoneNumber, userAddress, "Trainer", userEmergencyContactName, userEmergencyContactPhoneNumber);
     }
 }
