e37b2f55e4087549a7607c2343e61da1da89e2e0
JWilliams-Keyin <Jack.Williams@keyin.com>
2025-04-03 14:27:20 -0230

    Added constructor to Trainer.java

--- src/main/java/org/keyin/user/childclasses/Trainer.java
+++ src/main/java/org/keyin/user/childclasses/Trainer.java
@@ -3,4 +3,10 @@ package org.keyin.user.childclasses;
 import org.keyin.user.User;
 
 public class Trainer extends User {
+  
+    // Constructor
+
+    public Trainer(String username, String password, String email, int phoneNumber, String address) {
+        super(username, password, email, phoneNumber, address, "Trainer");
+    }
 }
