a52bd04aefd192cfe332b89cb0e4b95cc7d45d54
JWilliams-Keyin <Jack.Williams@keyin.com>
2025-04-04 18:38:26 -0230

    Added code for WorkoutClass.java

--- src/main/java/org/keyin/workoutclasses/WorkoutClass.java
+++ src/main/java/org/keyin/workoutclasses/WorkoutClass.java
@@ -1,4 +1,54 @@
 package org.keyin.workoutclasses;
 
 public class WorkoutClass {
+
+    // Attributes
+
+    private String gymClassType;
+    private String gymClassDescription;
+    private int trainerId;
+    
+    // Constructors
+    
+    public WorkoutClass(String gymClassType, String gymClassDescription, int trainerId) {
+        this.gymClassType = gymClassType;
+        this.gymClassDescription = gymClassDescription;
+        this.trainerId = trainerId;
+    }
+    
+    // Getters & Setters
+
+
+    public String getGymClassType() {
+        return gymClassType;
+    }
+
+    public String getGymClassDescription() {
+        return gymClassDescription;
+    }
+
+    public int getTrainerId() {
+        return trainerId;
+    }
+
+    public void setGymClassType(String gymClassType) {
+        this.gymClassType = gymClassType;
+    }
+
+    public void setGymClassDescription(String gymClassDescription) {
+        this.gymClassDescription = gymClassDescription;
+    }
+
+    public void setTrainerId(int trainerId) {
+        this.trainerId = trainerId;
+    }
+    
+    // Methods
+    
+    @Override
+    public String toString() {
+        return "Class Type: " + gymClassType + 
+                ", Class Description: " + gymClassDescription +
+                ", Trainer ID: " + trainerId;
+    }
 }
