5c9c023db09585374dd98a25805d131c64277f1f
Zackery Strickland <Zachary.Strickland@keyin.com>
2025-04-09 13:51:10 -0230

    Updated the enum to display prices. Updated the java to auto get the cost from enum

--- src/main/java/org/keyin/memberships/Membership.java
+++ src/main/java/org/keyin/memberships/Membership.java
@@ -1,7 +1,75 @@
 package org.keyin.memberships;
 
-//*\
-// This is class file that represents a membership
-//
 public class Membership {
+
+
+    private int membershipID;
+    private MembershipType membershipType;
+    private String membershipDescription;
+    private double membershipCost;
+    private int memberID;
+
+
+    public Membership(int membershipID, MembershipType membershipType, String membershipDescription, int memberID) {
+        this.membershipID = membershipID;
+        this.membershipType = membershipType;
+        this.membershipDescription = membershipDescription;
+        this.membershipCost = membershipType.getCost();
+        this.memberID = memberID;
+    }
+
+
+    public int getMembershipID() {
+        return membershipID;
+    }
+
+    public void setMembershipID(int membershipID) {
+        this.membershipID = membershipID;
+    }
+
+
+    public MembershipType getMembershipType() {
+        return membershipType;
+    }
+
+    public void setMembershipType(MembershipType membershipType) {
+        this.membershipType = membershipType;
+        this.membershipCost = membershipType.getCost(); // Recalculate cost if type changes
+    }
+
+
+    public String getMembershipDescription() {
+        return membershipDescription;
+    }
+
+    public void setMembershipDescription(String membershipDescription) {
+        this.membershipDescription = membershipDescription;
+    }
+
+
+    public double getMembershipCost() {
+        return membershipCost;
+    }
+
+
+    public int getMemberID() {
+        return memberID;
+    }
+
+    public void setMemberID(int memberID) {
+        this.memberID = memberID;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Membership{" +
+                "membershipID=" + membershipID +
+                ", membershipType=" + membershipType +
+                ", membershipDescription='" + membershipDescription + '\'' +
+                ", membershipCost=" + membershipCost +
+                ", memberID=" + memberID +
+                '}';
+    }
 }
+
---
+++ src/main/java/org/keyin/memberships/MembershipType.java
@@ -0,0 +1,25 @@
+package org.keyin.memberships;
+
+public enum MembershipType {
+    MONTHLY(30.0, false),
+    YEARLY(300.0, false),
+    MONTHLY_PREMIUM(50.0, true),
+    YEARLY_PREMIUM(500.0, true);
+
+    private final double cost;
+    private final boolean includesWorkoutAccess;
+
+    MembershipType(double cost, boolean includesWorkoutAccess) {
+        this.cost = cost;
+        this.includesWorkoutAccess = includesWorkoutAccess;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public boolean includesWorkoutAccess() {
+        return includesWorkoutAccess;
+    }
+}
+
